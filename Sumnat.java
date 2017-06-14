1c3b70cab8d91de4a880bf321012d2a33d542e6d
Mithunganesh <mithunganesh1@gmail.com>
2017-06-14 10:08:52 +0530

    Create Sumnat.java

---
+++ Sumnat.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Sumnat{
+    public static void main(String ar[]){
+        Scanner s=new Scanner(System.in);
+        int i1=s.nextInt();
+        int k=0;
+        for(int i=1;i<=i1;i++){
+             k=k+i;
+        }
+        System.out.println(k);
+        
+    }
+}
+
