cc03820268a7cdb5d28359b27e30fa47448d4153
Mithunganesh <mithunganesh1@gmail.com>
2017-06-14 10:02:25 +0530

    Create Print5time.java

---
+++ Print5time.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Print5time{
+    public static void main(String ar[]){
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        for(int i=0;i<5;i++){
+            System.out.println(s1);
+        }
+        
+    }
+}
+
