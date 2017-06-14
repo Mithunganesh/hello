050e3b45dff2027511e84660f9ac65456d5627bc
Mithunganesh <mithunganesh1@gmail.com>
2017-06-14 09:48:46 +0530

    Create C_alphabet.java

---
+++ C_alphabet.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Vowelorn{
+    public static void main(String arr[]){
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+       char k=s1.charAt(0);
+        if(Character.isLetter(k)){
+         System.out.println("Given Character is an Alphabet");
+         }else{
+         System.out.println("Given Character is not an Alphabet");
+         }
+      }
+}
