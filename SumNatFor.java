b0cf9144bedf716be7127487b6ef744b52ab6de7
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-06-15 18:19:24 +0530

    Create SumNatFor.java

--- SumNatFor.java
+++ SumNatFor.java
@@ -9,13 +9,11 @@ public class SumNatFor
         System.out.println("Enter Number of items :");
         Scanner s = new Scanner(System.in);
         int x = s.nextInt();
-        if(x>1){
-        for(i=1;i<=x;i++){
+        
+        for(i=0;i<=x;i++){
             sum = sum +i;
         }
         System.out.println("Sum of "+x+" numbers is :"+sum);
-        }else{
-            System.out.println("Enter the Natural number...");
-        }
+       
     }
 }
