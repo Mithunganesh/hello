dc85ba556c9920a9838ebf6651c2c99bb3245ad4
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-06-15 18:09:57 +0530

    Create Natural.java

--- Natural.java
+++ Natural.java
@@ -9,11 +9,15 @@ public class Natural
         System.out.println("Enter Number of items :");
         Scanner s = new Scanner(System.in);
         x = s.nextInt();
+        if(x<1){
         while(i <= x)
         {
             sum = sum +i;
             i++;
         }
         System.out.println("Sum of "+x+" numbers is :"+sum);
+        }else{
+            System.out.println("Enter the Natural number...");
+        }
     } 
 }
