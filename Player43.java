6fcb4d05f304933eb8e84ffa525fd129b3022ffd
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-06-21 11:19:35 +0530

    Create Player43.java

---
+++ Player43.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Player43{
+public static void main(String arr[]){
+Scanner scan=new Scanner(System.in);
+String s1=scan.nextLine();
+String s2=scan.nextLine();
+if(s1.contains(s2)){
+System.out.println("yes");
+}else{
+System.out.println("no");}
+}
+}
