666164ec622b26f8d2519154619b048f38240aef
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-06-21 15:50:08 +0530

    Update Beginner14.java

--- Beginner14.java
+++ Beginner14.java
@@ -4,10 +4,14 @@ class Beginner14{
         Scanner s=new Scanner(System.in);
        int start=s.nextInt();
        int end=s.nextInt();
+       if(start!=end){
        for(int i=start;i<=end;i++){
            if(i%2!=0){
             System.out.println(i);   
            }
+       }
+       }else{
+           System.out.println("Enter the proper intervals");
        }
     }  
 }
