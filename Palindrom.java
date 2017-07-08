1618f0ae4229d727275b2388065cb76431aba4b0
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-07-08 21:50:59 +0530

    Update Palindrom.java

--- Palindrom.java
+++ Palindrom.java
@@ -1,41 +1,32 @@
-import java.io.*;
-import java.util.*;
-class Palindrom{
-    public static void main(String arr[]){
-        Scanner ss=new Scanner(System.in);
-        String s1=ss.nextLine();
-        String res="";
-        res=longestPalin(s1);
-        System.out.println(res);
-   
-    }
-        
-        public static String longestPalin(String s) {
-    if(s==null || s.length()<=1)
-        return s;
- 
-    int ll = s.length();
-    int mL = 1;
-    boolean [][] dp = new boolean[ll][ll];
- 
-    String longpal = null;
-    for(int l=0; l<s.length(); l++){
-        for(int i=0; i<ll-l; i++){
-            int j = i+l;
-            if(s.charAt(i)==s.charAt(j) && (j-i<=2||dp[i+1][j-1])){
-                dp[i][j]=true;
- 
-                if(j-i+1>mL){
-                   mL = j-i+1; 
-                   longpal = s.substring(i, j+1);
-                }
-            }
-        }
-    }
- 
-    return longpal;
-        }
-}
-    
 
 
+		import java.util.*;
+		import java.lang.*;
+		import java.io.*;
+
+		
+		class Palindrome{
+			public static void main (String[] args) throws java.lang.Exception
+			{
+				Scanner sc=new Scanner(System.in);
+				String input=sc.next();
+				String palindrome=new String();
+				int length=0;
+				for(int i=0;i<input.length();i++)
+				{
+				    for(int j=i+1;j<input.length();j++)
+				    {
+				        String temp=input.substring(i,j);
+				        if(temp.equalsIgnoreCase(String.valueOf(new StringBuffer(temp).reverse())))
+				        {
+				            if(temp.length()>length)
+				            {
+				                length=temp.length();
+				                palindrome=new String(temp);
+				            }
+				        }
+				    }
+				}
+				System.out.println(palindrome);
+			}
+		}
