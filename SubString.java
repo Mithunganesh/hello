13f417971784b1c6f93c161ba14ec632c52db6e9
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-07-08 21:58:50 +0530

    Update SubString.java

--- SubString.java
+++ SubString.java
@@ -1,28 +1,28 @@
-import java.util.HashSet;
-import java.util.*;
-import java.io.*;
-public class SubString {
-    public static String subString(String input){
-        HashSet<Character> set = new HashSet<Character>();
-        String longestsubstr = "";
-        String longestnow = "";
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (set.contains(c)) {
-                longestNow = "";
-                set.clear();
-            }
-            longestNow += c;
-            set.add(c);
-            if (longestNow.length() > longestsubstr.length()) {
-                longestsubstr = longestNow;
-            }
-        }
-        return longestsubstr;
-    }
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        String input=s.nextLine();
-        System.out.println(subString(input));
-    }
-}
+    import java.util.Scanner;
+	import java.util.HashSet;
+		public class arry {
+		    public static String subString(String input){
+		        HashSet<Character> set = new HashSet<Character>();
+		        String longestsubstr = "";
+		        String longestnow = "";
+		        for (int i = 0; i < input.length(); i++) {
+		            char c = input.charAt(i);
+		            if (set.contains(c)) {
+		                longestnow = "";
+		                set.clear();
+		            }
+		            longestnow += c;
+		            set.add(c);
+		            if (longestnow.length() > longestsubstr.length()) {
+		                longestsubstr = longestnow;
+		            }
+		        }
+		        return longestsubstr;
+		    }
+		    public static void main(String[] args) {
+		        Scanner s=new Scanner(System.in);
+		        String input=s.nextLine();
+		        System.out.println(subString(input));
+		        s.close();
+		    }
+		}
